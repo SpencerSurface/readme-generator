039d288c2ee518e53af299448102e00303a511af
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-21 14:13:26 -0500

    add array of questions for user input

--- index.js
+++ index.js
@@ -2,8 +2,62 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// DONE: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a description for the project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Describe how to install the project:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Describe how to use the project:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Provide guidelines on how to contribute to the project:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Enter any instructions about how to test the project:",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What license does your project use?",
+        name: "license",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv2",
+            "GNU GPLv3",
+            "ISC License",
+            "MIT License",
+            "Unlicense"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
